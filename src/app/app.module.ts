7ba6be96a90ff50554a7d7a82fe3184cd64c7347
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-07-22 20:03:43 +0300

    Make html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldNgIfComponent } from './hello-world-ngIf/hello-world-ngIf.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    HelloWorldNgIfComponent
   ],
   imports: [
     BrowserModule
